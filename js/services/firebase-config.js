074a5cde2c91273d2bd3a0c3adc22b7f2882fb13
berat-arsln <beratarsln@gmail.com>
2026-06-08 23:39:29 +0300

    Update firebase-config.js

--- js/services/firebase-config.js
+++ js/services/firebase-config.js
@@ -1,1 +1,14 @@
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGELLtIOdSbNbBasEOjI53wTMcY9GlD6Y",
+  authDomain: "passaword-3c769.firebaseapp.com",
+  databaseURL: "https://passaword-3c769-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "passaword-3c769",
+  storageBucket: "passaword-3c769.firebasestorage.app",
+  messagingSenderId: "184241248120",
+  appId: "1:184241248120:web:1e56d87fefd25b0971da8d"
+};
+
+export const app = initializeApp(firebaseConfig);
